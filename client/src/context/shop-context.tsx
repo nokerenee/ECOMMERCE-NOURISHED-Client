06ae660872781451c837f59aff40e55a004b6677
Nicole Kelly <nrkelly57@gmail.com>
2024-01-03 19:14:01 +1300

    Added cart item logic and button functionality

--- client/src/context/shop-context.tsx
+++ client/src/context/shop-context.tsx
@@ -1,10 +1,17 @@
 import { createContext, useState } from "react";
+import { useGetProducts } from "../hooks/useGetProducts";
+import { IProduct } from "../models/interfaces";
+import { useGetToken } from "../hooks/useGetToken";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export interface IShopContext {
   addToCart: (itemId: string) => void;
   removeFromCart: (itemId: string) => void;
   updateCartItemCount: (newAmount: number, itemId: string) => void;
   getCartItemCount: (itemId: string) => number;
+  getTotalCartAmount: () => number;
+  checkout: () => void;
 }
 
 const defaultVal: IShopContext = {
@@ -12,12 +19,17 @@ const defaultVal: IShopContext = {
   removeFromCart: () => null,
   updateCartItemCount: () => null,
   getCartItemCount: () => 0,
+  getTotalCartAmount: () => 0,
+  checkout: () => null,
 };
 
 export const ShopContext = createContext<IShopContext>(defaultVal);
 
 export const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState<{ string: number } | {}>({});
+  const { products } = useGetProducts();
+  const { headers } = useGetToken();
+  const navigate = useNavigate();
 
   const getCartItemCount = (itemId: string): number => {
     if (itemId in cartItems) {
@@ -34,15 +46,50 @@ export const ShopContextProvider = (props) => {
     }
   };
 
-  const removeFromCart = (itemId: string) => {};
+  const removeFromCart = (itemId: string) => {
+    if (!cartItems[itemId]) return;
+    if (cartItems[itemId] == 0) return;
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
 
-  const updateCartItemCount = (newAmount: number, itemId: string) => {};
+  const updateCartItemCount = (newAmount: number, itemId: string) => {
+    if (newAmount < 0) return;
+    setCartItems((prev) => ({ ...prev, [itemId]: newAmount }));
+  };
+
+  const getTotalCartAmount = (): number => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo: IProduct = products.find(
+          (product) => product._id === item
+        );
+
+        totalAmount += cartItems[item] * itemInfo.price;
+      }
+    }
+    return totalAmount;
+  };
+
+  const checkout = async () => {
+    const body = { customerID: localStorage.getItem("userID"), cartItems };
+    try {
+      await axios.post("http://localhost:3001/products/checkout", body, {
+        headers,
+      });
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const contextValue: IShopContext = {
     addToCart,
     removeFromCart,
     updateCartItemCount,
     getCartItemCount,
+    getTotalCartAmount,
+    checkout,
   };
 
   return (
--- client/src/pages/checkout/index.tsx
+++ client/src/pages/checkout/index.tsx
@@ -4,10 +4,15 @@ import { IProduct } from "../../models/interfaces";
 import { IShopContext, ShopContext } from "../../context/shop-context";
 import { CartItem } from "./cart-item";
 import "./styles.css";
+import { useNavigate } from "react-router-dom";
 
 export const CheckoutPage = () => {
-  const { getCartItemCount } = useContext<IShopContext>(ShopContext);
+  const { getCartItemCount, getTotalCartAmount, checkout } =
+    useContext<IShopContext>(ShopContext);
   const { products } = useGetProducts();
+  const totalAmount = getTotalCartAmount();
+  const navigate = useNavigate();
+
   return (
     <div className="cart">
       <div>
@@ -20,6 +25,16 @@ export const CheckoutPage = () => {
           }
         })}
       </div>
+
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p>Subtotal: ${totalAmount.toFixed(2)}</p>
+          <button onClick={() => navigate("/")}>Continue Shopping</button>
+          <button onClick={checkout}>Checkout</button>
+        </div>
+      ) : (
+        <h1>Your Shopping Cart is Empty</h1>
+      )}
     </div>
   );
 };
