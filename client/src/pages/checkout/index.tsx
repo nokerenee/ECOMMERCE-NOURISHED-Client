a12fe8ef1bd73a99390ce974be03742cf846dcdc
Nicole Kelly <nrkelly57@gmail.com>
2024-01-06 22:31:57 +1300

    package reinstall and styling

--- client/src/pages/checkout/index.tsx
+++ client/src/pages/checkout/index.tsx
@@ -16,32 +16,35 @@ export const CheckoutPage = () => {
 
   return (
     <div className="cart">
-      <div>
-        <h1>Shopping Cart</h1>
-      </div>
-      <div className="cart">
-        {products.map((product: IProduct) => {
-          if (getCartItemCount(product._id) !== 0) {
-            return <CartItem key={product._id} product={product} />;
-          }
-        })}
-      </div>
-
-      {totalAmount > 0 ? (
-        <div className="checkout">
-          <p>Subtotal: ${totalAmount.toFixed(2)}</p>
-          <button onClick={() => navigate("/")}>Continue Shopping</button>
-          <button
-            onClick={() => {
-              checkout(localStorage.getItem("userID"));
-            }}
-          >
-            Checkout
-          </button>
+      <section className="cart-banner"></section>
+      <div className="cart-page">
+        <div>
+          <h1>Shopping Cart</h1>
         </div>
-      ) : (
-        <h1>Your Shopping Cart is Empty</h1>
-      )}
+        <div className="cart-page">
+          {products.map((product: IProduct) => {
+            if (getCartItemCount(product._id) !== 0) {
+              return <CartItem key={product._id} product={product} />;
+            }
+          })}
+        </div>
+
+        {totalAmount > 0 ? (
+          <div className="checkout">
+            <p>Subtotal: ${totalAmount.toFixed(2)}</p>
+            <button onClick={() => navigate("/")}>Continue Shopping</button>
+            <button
+              onClick={() => {
+                checkout(localStorage.getItem("userID"));
+              }}
+            >
+              Checkout
+            </button>
+          </div>
+        ) : (
+          <h2>Your Shopping Cart is Empty</h2>
+        )}
+      </div>
     </div>
   );
 };
--- client/src/pages/purchased-items/index.tsx
+++ client/src/pages/purchased-items/index.tsx
@@ -7,23 +7,26 @@ export const PurchasedItemsPage = () => {
     useContext(ShopContext);
 
   return (
-    <div className="purchased-items-page">
-      <h1>Previously Purchased Items</h1>
-      <div className="purchased-items">
-        {purchasedItems.map((item) => {
-          const cartItemCount = getCartItemCount(item._id);
+    <div className="purchase">
+      <section className="purchase-banner"></section>
+      <div className="purchased-items-page">
+        <h1>Previously Purchased Items</h1>
+        <div className="purchased-items">
+          {purchasedItems.map((item) => {
+            const cartItemCount = getCartItemCount(item._id);
 
-          return (
-            <div key={item._id} className="item">
-              <h3>{item.productName}</h3>
-              <img src={item.imageURL} alt={item.productName} />
-              <p>${item.price}</p>
-              <button onClick={() => addToCart(item._id)}>
-                Purchase Again {cartItemCount > 0 && <>({cartItemCount})</>}
-              </button>
-            </div>
-          );
-        })}
+            return (
+              <div key={item._id} className="item">
+                <h3>{item.productName}</h3>
+                <img src={item.imageURL} alt={item.productName} />
+                <p>${item.price}</p>
+                <button onClick={() => addToCart(item._id)}>
+                  Purchase Again {cartItemCount > 0 && <>({cartItemCount})</>}
+                </button>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
